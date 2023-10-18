95f04f3701e9c82462cabc00398d0b7e4b14f55c
fullstackdev3546 <fullstackdev3546@gmail.com>
2023-10-19 00:13:34 +0900

    solidity

---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: "0.8.19",
+  },
+  paths:{
+    artifacts: './frontend/src/artifacts'
+  },
+  networks:{
+    hardhat: {
+      chainId: 1337,
+    }
+  }
+
+};
